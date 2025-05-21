1e2f0f4aaa9785c44cf27c0ded94c55ed4e9b6c3
Johnny Andreas Malmedal <143591196+JohnnyDisk@users.noreply.github.com>
2025-05-21 17:40:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <h1>Welcome to OnlineBank</h1>
   <ul>
     <li><a href="Bank_Oppgave/login.php">Login</a></li>
-    <li><a href="Bank_Oppgave/register.php">Register</a></li>
+    <li><a href="Bank_Oppgave/signup.php">Signup</a></li>
     <li><a href="Bank_Oppgave/index.php">Index</a></li>
     <li><a href="Bank_Oppgave/admin_panel.php">Admin Panel</a></li>
   </ul>

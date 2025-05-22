227f2b5bc0fb5352cac508a88ec7313c5698f7d1
Johnny Andreas Malmedal <143591196+JohnnyDisk@users.noreply.github.com>
2025-05-22 18:11:06 +0200

    Update connection.php

--- Bank_Oppgave/connection.php
+++ Bank_Oppgave/connection.php
@@ -6,6 +6,18 @@ $dbhost = "onlinebank-server.mysql.database.azure.com";
 $dbuser = "JohnnyDisk";
 $dbpass = "IMKuben1337!";
 $dbname = "onlinebank-database";
+$ssl_cert = __DIR__ . "/certs/azure-ca.pem";
+
+// Create a connection using SSL
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, $ssl_cert, NULL, NULL);
+mysqli_real_connect($conn, $dbhost, $dbuser, $dbpass, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
+
+if (mysqli_connect_errno()) {
+    die("Failed to connect: " . mysqli_connect_error());
+} else {
+    echo "Connected with SSL!";
+}
 
 # Localhost
 
@@ -21,8 +33,8 @@ $dbname = "onlinebank-database";
 # $dbpass = "IMKuben1337!";
 # $dbname = "Bank_DB";
 
-if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
-{
-    die("failed to connect!");
-}
-
+# if(!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname))
+# {
+#     die("failed to connect!");
+# }
+?>

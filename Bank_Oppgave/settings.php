cfc8b245e740226b1e963c3fd5f12eaaf442e47d
JohnnyDisk <143591196+JohnnyDisk@users.noreply.github.com>
2025-06-12 11:05:26 +0200

    test

--- Bank_Oppgave/settings.php
+++ Bank_Oppgave/settings.php
@@ -7,6 +7,7 @@ include('functions.php');
 $user_data = check_login($con);
 
 if($_SERVER['REQUEST_METHOD'] == "post"){
+    echo "test";
     $new_name = $_POST['name'];
     $user_id = $user_data['user_id'];
 

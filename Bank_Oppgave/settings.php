b82b4a515dc889af6dff0600e711bd2bb3f7e43c
JohnnyDisk <143591196+JohnnyDisk@users.noreply.github.com>
2025-06-12 11:22:29 +0200

    update

--- Bank_Oppgave/settings.php
+++ Bank_Oppgave/settings.php
@@ -8,6 +8,8 @@ $user_data = check_login($con);
 
 if($_SERVER['REQUEST_METHOD'] == "POST"){
     $new_name = $_POST['name'];
+    $new_password = $_POST['password'];
+    $password_verify = $_POST['password_verify'];
     $user_id = $user_data['user_id'];
 
     $safe_name = mysqli_escape_string($con, $new_name);
@@ -20,6 +22,17 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
         }
     }
 
+    $safe_password = mysqli_escape_string($con, $new_password);
+    if(!empty($new_password) && $new_password == $password_verify && password_verify($new_password, $user_data['password'])){
+        $hashed_password = password_hash($safe_password, PASSWORD_DEFAULT);
+        $query = "UPDATE users SET password = '$safe_password' WHERE user_id = '$user_id'";
+        if(mysqli_query($con, $query)){
+            echo "password change successfully";
+        } else {
+            echo "error in password change";
+        }
+    }
+
 
 }
 
@@ -29,7 +42,10 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 <body>
 <form method="post">
 <label for="name">Name:</label>
-<input type="text" name="name" placeholder="name: ">
+<input type="text" name="name" placeholder="name: "> <br>
+<label for="name">Password:</label>
+<input type="password" name="password" placeholder="password: ">
+<input type="password_verify" name="password" placeholder="verify password: ">
 <input type="submit" value="Submit">
 </form>
 </body>

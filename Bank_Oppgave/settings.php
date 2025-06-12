b938ad5f0a6bb07fd4f0bd6bd9f24de261cd8c7e
JohnnyDisk <143591196+JohnnyDisk@users.noreply.github.com>
2025-06-12 11:03:20 +0200

    update settings

---
+++ Bank_Oppgave/settings.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+
+include('connection.php');
+include('functions.php');
+
+$user_data = check_login($con);
+
+if($_SERVER['REQUEST_METHOD'] == "post"){
+    $new_name = $_POST['name'];
+    $user_id = $user_data['user_id'];
+
+    $safe_name = mysqli_escape_string($con, $new_name);
+    if(!empty($new_name) && !is_numeric($new_name)){
+        $query = "UPDATE users SET name = '$safe_name' WHERE user_id = '$user_id";
+        if(mysqli_query($con, $query)){
+            echo "name change successfully";
+        } else {
+            echo "error in name change";
+        }
+    }
+
+
+}
+
+?>
+
+<html>
+<body>
+<form method="post">
+<label for="name">Name:</label>
+<input type="text" name="name" placeholder="name: ">
+<input type="submit" value="Submit">
+</form>
+</body>
+</html>
